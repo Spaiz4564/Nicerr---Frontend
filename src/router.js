a309b4ee9d7d7aa0110da30a97413ff54e14f80a
ylcN101 <yuvalcohen12234@gmail.com>
2023-03-21 20:40:05 +0200

    wiring up

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
 import Chat from './views/Chat.vue'
-import CarIndex from './views/CarIndex.vue'
+import GigIndex from './views/GigIndex.vue'
 import ReviewIndex from './views/ReviewIndex.vue'
 import LoginSignup from './views/LoginSignup.vue'
 import UserDetails from './views/UserDetails.vue'
@@ -11,39 +11,37 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/car',
-    name: 'CarIndex',
-    component: CarIndex
+    path: '/gig',
+    name: 'GigIndex',
+    component: GigIndex,
   },
   {
     path: '/review',
     name: 'ReviewIndex',
-    component: ReviewIndex
+    component: ReviewIndex,
   },
   {
     path: '/chat',
     name: 'Chat',
-    component: Chat
+    component: Chat,
   },
   {
     path: '/login',
     name: 'LoginSignup',
-    component: LoginSignup
+    component: LoginSignup,
   },
   {
     path: '/user/:id',
     name: 'UserDetails',
-    component: UserDetails
-  }
+    component: UserDetails,
+  },
 ]
-
 
 export const router = createRouter({
   routes,
-  history: createWebHashHistory()
+  history: createWebHashHistory(),
   // base: process.env.BASE_URL,
 })
-
