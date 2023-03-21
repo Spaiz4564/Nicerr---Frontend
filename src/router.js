98d41052a0aadbb7fb62746af772f08786a59c7a
Lidor Tal <lidortald@gmail.com>
2023-03-21 22:57:30 +0200

    adding route to Details

--- src/router.js
+++ src/router.js
@@ -1,47 +1,53 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import Home from './views/Home.vue'
-import Chat from './views/Chat.vue'
-import GigIndex from './views/GigIndex.vue'
-import ReviewIndex from './views/ReviewIndex.vue'
-import LoginSignup from './views/LoginSignup.vue'
-import UserDetails from './views/UserDetails.vue'
+import Home from "./views/Home.vue";
+import Chat from "./views/Chat.vue";
+import GigIndex from "./views/GigIndex.vue";
+import GigDetails from "./views/GigDetails.vue";
+import ReviewIndex from "./views/ReviewIndex.vue";
+import LoginSignup from "./views/LoginSignup.vue";
+import UserDetails from "./views/UserDetails.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/gig',
-    name: 'GigIndex',
+    path: "/gig",
+    name: "GigIndex",
     component: GigIndex,
   },
   {
-    path: '/review',
-    name: 'ReviewIndex',
+    path: "/gig/:id",
+    name: "gigDetails",
+    component: GigDetails,
+  },
+  {
+    path: "/review",
+    name: "ReviewIndex",
     component: ReviewIndex,
   },
   {
-    path: '/chat',
-    name: 'Chat',
+    path: "/chat",
+    name: "Chat",
     component: Chat,
   },
   {
-    path: '/login',
-    name: 'LoginSignup',
+    path: "/login",
+    name: "LoginSignup",
     component: LoginSignup,
   },
   {
-    path: '/user/:id',
-    name: 'UserDetails',
+    path: "/user/:id",
+    name: "UserDetails",
     component: UserDetails,
   },
-]
+];
 
 export const router = createRouter({
   routes,
   history: createWebHashHistory(),
   // base: process.env.BASE_URL,
-})
+});
