2ec47db881c3c5393cb63b0a82c3da205e7c832d
Guy <guyshilon30@gmail.com>
2023-03-22 22:10:21 +0200

    more homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,14 @@
   <HeroSection />
   <TrustedBy />
   <PopularServices />
+  <About />
 </template>
 
 <script>
 import HeroSection from '../cmps/HeroSection.vue'
 import TrustedBy from '../cmps/TrustedBy.vue'
 import PopularServices from '../cmps/PopularServices.vue'
+import About from '../cmps/About.vue'
 
 export default {
   name: 'home',
@@ -19,6 +21,7 @@ export default {
     HeroSection,
     TrustedBy,
     PopularServices,
+    About,
   },
 }
 </script>
