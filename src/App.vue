bdca725f289ebf5ffc464232b5a5731dafdd0e8d
Guy <guyshilon30@gmail.com>
2023-03-22 12:30:30 +0200

    hero&header

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <section>
+  <div class="main-layout">
     <AppHeader />
-    <RouterView/>
-    <UserMsg/>
-  </section>
+    <RouterView />
+    <UserMsg />
+  </div>
 </template>
 
 <script>
-
-
 import { userService } from './services/user.service'
 import { store } from './store/store'
 
 import AppHeader from './cmps/AppHeader.vue'
 import UserMsg from './cmps/UserMsg.vue'
 
-
 export default {
-
   created() {
     console.log('Vue App created')
     const user = userService.getLoggedinUser()
-    if (user)  store.commit({type: 'setLoggedinUser', user})
+    if (user) store.commit({ type: 'setLoggedinUser', user })
   },
   components: {
     AppHeader,
-    UserMsg
+    UserMsg,
   },
 }
 </script>
