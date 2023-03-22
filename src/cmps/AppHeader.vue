219ff81607fdb44df1082d69f69c4165724f7273
Lidor Tal <lidortald@gmail.com>
2023-03-22 22:25:03 +0200

    starting observer

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <header>
+  <header ref="header">
     <div></div>
-    <nav>
+    <nav ref="nav" v-bind:style="{ position: stickyNav ? 'static' : 'absolute' }">
       <h1 class="logo">Nicerr<span>.</span></h1>
       <div class="goTo">
         <RouterLink to="/gig">Explore</RouterLink>
@@ -10,12 +10,34 @@
         <a>Join</a>
       </div>
     </nav>
-
     <div></div>
   </header>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      stickyNav: false
+    }
+  },
   computed: {},
+  methods: {
+    onHeaderObserved(entries) {
+      entries.forEach(entry => {
+        this.stickyNav = entry.isIntersecting ? true : false
+        console.log(entry);
+        console.log('hello');
+
+      });
+    }
+  },
+  mounted() {
+    this.headerObserver = new IntersectionObserver(this.onHeaderObserved, {
+      rootMargin: "100px 0px 0px"
+    })
+    this.headerObserver.observe(this.$refs.header)
+    console.log('hello -mounted');
+
+  }
 }
 </script>
