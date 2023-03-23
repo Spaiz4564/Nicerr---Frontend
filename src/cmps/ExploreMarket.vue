d6c56a318a2e27907fef4cac2db9f6e6c7883846
Guy <guyshilon30@gmail.com>
2023-03-23 13:44:34 +0200

    svg change

---
+++ src/cmps/ExploreMarket.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="explore-market-section">
+        <h2>Explore the marketplace</h2>
+<div class="categories-list">
+<div>
+    <span></span>
+    <h4></h4>
+</div>
+</div>
+    </section>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: 'Explore Market',
+    data() {
+      return {}
+    },
+    methods: {},
+    components: {
+      
+    },
+  }
+  </script>
+  
