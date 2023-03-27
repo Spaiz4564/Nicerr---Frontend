22e01819199ce2b1a46c61b71738aeb2af33b5fe
Lidor Tal <lidortald@gmail.com>
2023-03-27 20:02:02 +0300

    .

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -1,35 +1,33 @@
 <template>
   <section class="main-header main-layout">
     <header class="main-layout" :class="[isHome ? 'headerHome' : '', isWhite ? 'homeScroll' : '']" ref="header">
-      <section class="main-header main-layout">
-        <header class="main-layout" :class="[isHome ? 'headerHome' : '', isWhite ? 'homeScroll' : '']" ref="header">
-          <nav>
-            <div class="logo-search">
-              <RouterLink to="/">
-                <h1 class="logo">Nicerr</h1>
-              </RouterLink>
-              <form @submit.prevent="emitFiltered" v-if="!isHome || isSuggestions" class="search-bar">
-                <input class="search-input" type="text" placeholder="What service are you looking for today?"
-                  v-model="filterBy.title" />
-                <span class="icon-search" v-html="getSvg('search')"></span>
-              </form>
-            </div>
-            <div class="goTo">
-              <RouterLink to="/gig">Explore</RouterLink>
-              <a @click="goToSellerSignup">Become a Seller</a>
-              <a v-if="!loggedinUser">Sign In</a>
-              <a v-if="!loggedinUser">Join</a>
-            </div>
-            <div class="modal" v-if="loggedinUser">
-              <img class="user-img" :src="loggedinUser.imgUrl" alt="user-img" @click.stop="toggleUserModal" />
-              <div class="user-modal" v-if="modalOpen">
-                <a @click="goToProfile">Profile</a>
-              </div>
-            </div>
-          </nav>
-        </header>
-        <NavSuggestions :isWhite="isWhite" :isSuggestions="isSuggestions" :isHome="isHome" />
-      </section>
+      <nav>
+        <div class="logo-search">
+          <RouterLink to="/">
+            <h1 class="logo">Nicerr</h1>
+          </RouterLink>
+          <form @submit.prevent="emitFiltered" v-if="!isHome || isSuggestions" class="search-bar">
+            <input class="search-input" type="text" placeholder="What service are you looking for today?"
+              v-model="filterBy.title" />
+            <span class="icon-search" v-html="getSvg('search')"></span>
+          </form>
+        </div>
+        <div class="goTo">
+          <RouterLink to="/gig">Explore</RouterLink>
+          <a @click="goToSellerSignup">Become a Seller</a>
+          <a v-if="!loggedinUser">Sign In</a>
+          <a v-if="!loggedinUser">Join</a>
+        </div>
+        <div class="modal" v-if="loggedinUser">
+          <img class="user-img" :src="loggedinUser.imgUrl" alt="user-img" @click.stop="toggleUserModal" />
+          <div class="user-modal" v-if="modalOpen">
+            <a @click="goToProfile">Profile</a>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <NavSuggestions :isWhite="isWhite" :isSuggestions="isSuggestions" :isHome="isHome" />
+  </section>
 </template>
 <script>
 import { svgService } from '../services/svg.service'
