e4d541015395fbaf9d06252827224b515f2e4aed
Guy <guyshilon30@gmail.com>
2023-03-29 21:01:52 +0300

    profile dropdown

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -3,7 +3,8 @@
     <header
       class="main-layout"
       :class="[isHome ? 'headerHome' : '', isWhite ? 'homeScroll' : '']"
-      ref="header">
+      ref="header"
+    >
       <nav>
         <div class="logo-search">
           <RouterLink to="/">
@@ -12,12 +13,14 @@
           <form
             @submit.prevent="emitFiltered"
             v-if="!isHome || isSuggestions"
-            class="search-bar">
+            class="search-bar"
+          >
             <input
               class="search-input"
               type="text"
               placeholder="What service are you looking for today?"
-              v-model="filterBy.title" />
+              v-model="filterBy.title"
+            />
             <span class="icon-search" v-html="getSvg('search')"></span>
           </form>
         </div>
@@ -36,9 +39,10 @@
               class="user-img"
               :src="loggedinUser.imgUrl"
               alt="user-img"
-              @click.stop="toggleUserModal" />
+              @click.stop="toggleUserModal"
+            />
 
-              <!-- <div
+            <!-- <div
               v-clickOutsideDirective="closeUserMenu"
               class="orders-modal"
               v-if="modalOpen">
@@ -51,8 +55,13 @@
             <div
               v-clickOutsideDirective="closeUserMenu"
               class="user-modal"
-              v-if="modalOpen">
+              v-if="modalOpen"
+            >
+              <div class="modal-tip"></div>
               <a @click="goToProfile">Profile</a>
+              <a v-if="loggedinUser.isSeller" @click="goToDashboard"
+                >Dashboard</a
+              >
               <a @click="logout">Logout</a>
             </div>
           </div>
@@ -62,103 +71,107 @@
     <NavSuggestions
       :isWhite="isWhite"
       :isSuggestions="isSuggestions"
-      :isHome="isHome" />
+      :isHome="isHome"
+    />
   </section>
 </template>
 <script>
-import { svgService } from '../services/svg.service'
-import { gigService } from '../services/gig.service.local'
-import Login from '../views/Login.vue'
-import NavSuggestions from './NavSuggestions.vue'
-export default {
-  data() {
-    return {
-      stickyNav: false,
-      headerObserver: null,
-      filterBy: {
-        title: '',
+  import { svgService } from '../services/svg.service'
+  import { gigService } from '../services/gig.service.local'
+  import Login from '../views/Login.vue'
+  import NavSuggestions from './NavSuggestions.vue'
+  export default {
+    data() {
+      return {
+        stickyNav: false,
+        headerObserver: null,
+        filterBy: {
+          title: '',
+        },
+        isHome: true,
+        isWhite: false,
+        isSuggestions: false,
+        isPurchase: false,
+        categories: gigService.getMarketCategories(),
+        modalOpen: false,
+        modalSignIsOpen: false,
+        backdrop: document.querySelector('.backdrop'),
+      }
+    },
+    computed: {
+      loggedinUser() {
+        console.log('loggedinUser', this.$store.getters.loggedinUser)
+        return this.$store.getters.loggedinUser
       },
-      isHome: true,
-      isWhite: false,
-      isSuggestions: false,
-      isPurchase: false,
-      categories: gigService.getMarketCategories(),
-      modalOpen: false,
-      modalSignIsOpen: false,
-      backdrop: document.querySelector('.backdrop'),
-    }
-  },
-  computed: {
-    loggedinUser() {
-      console.log('loggedinUser', this.$store.getters.loggedinUser)
-      return this.$store.getters.loggedinUser
+      seller() {
+        if (!this.loggedinUser) return false
+        return this.loggedinUser.isSeller
+      },
     },
-    seller() {
-      if (!this.loggedinUser) return false
-      return this.loggedinUser.isSeller
+    methods: {
+      getSvg(iconName) {
+        return svgService.getSvg(iconName)
+      },
+      emitFiltered() {
+        //push the query to the url
+        this.$router.push({
+          path: '/gig',
+          query: { title: this.filterBy.title },
+        })
+      },
+      goToSellerSignup() {
+        this.$router.push('/seller-signup')
+      },
+      handleScroll() {
+        const scrollY = window.scrollY
+        this.isWhite = scrollY > 20 ? true : false
+        this.isSuggestions = scrollY > 170 ? true : false
+      },
+      toggleUserModal() {
+        this.modalOpen = !this.modalOpen
+      },
+      goToProfile() {
+        this.$router.push(`/seller/profile/${this.loggedinUser._id}`)
+      },
+      goToDashboard() {
+        console.log('dashboard')
+      },
+      filterCategory(categoryId) {
+        this.$router.push(`/gig/${categoryId}`)
+        this.$store.commit({ type: 'setFilter', filterBy: { categoryId } })
+      },
+      logout() {
+        this.$store.dispatch({ type: 'logout' })
+        this.$router.push('/')
+      },
+      toggleSignInModal() {
+        this.modalSignIsOpen = !this.modalSignIsOpen
+        this.$emit('backdrop', this.modalSignIsOpen, 'signIn')
+      },
+      toggleJoinModal() {
+        this.modalSignIsOpen = !this.modalSignIsOpen
+        this.$emit('backdrop', this.modalSignIsOpen, 'join')
+      },
+      closeUserMenu() {
+        this.modalOpen = false
+      },
     },
-  },
-  methods: {
-    getSvg(iconName) {
-      return svgService.getSvg(iconName)
+    watch: {
+      $route(to) {
+        this.isHome = to.path !== '/' ? false : true
+        this.isPurchase = to.path.includes('purchase') ? true : false
+      },
     },
-    emitFiltered() {
-      //push the query to the url
-      this.$router.push({
-        path: '/gig',
-        query: { title: this.filterBy.title },
-      })
+    created() {
+      window.addEventListener('scroll', this.handleScroll)
+      if (this.backdrop) {
+        this.backdrop.addEventListener('click', this.toggleSignInModal)
+      }
+      this.handleScroll()
     },
-    goToSellerSignup() {
-      this.$router.push('/seller-signup')
+    mounted() {
+      window.addEventListener('scroll', this.handleScroll)
     },
-    handleScroll() {
-      const scrollY = window.scrollY
-      this.isWhite = scrollY > 20 ? true : false
-      this.isSuggestions = scrollY > 170 ? true : false
-    },
-    toggleUserModal() {
-      this.modalOpen = !this.modalOpen
-    },
-    goToProfile() {
-      this.$router.push(`/seller/profile/${this.loggedinUser._id}`)
-    },
-    filterCategory(categoryId) {
-      this.$router.push(`/gig/${categoryId}`)
-      this.$store.commit({ type: 'setFilter', filterBy: { categoryId } })
-    },
-    logout() {
-      this.$store.dispatch({ type: 'logout' })
-      this.$router.push('/')
-    },
-    toggleSignInModal() {
-      this.modalSignIsOpen = !this.modalSignIsOpen
-      this.$emit('backdrop', this.modalSignIsOpen, 'signIn')
-    },
-    toggleJoinModal() {
-      this.modalSignIsOpen = !this.modalSignIsOpen
-      this.$emit('backdrop', this.modalSignIsOpen, 'join')
-    },
-    closeUserMenu() {
-      this.modalOpen = false
-    },
-  },
-  watch: {
-    $route(to) {
-      this.isHome = to.path !== '/' ? false : true
-      this.isPurchase = to.path.includes('purchase') ? true : false
-    },
-  },
-  created() {
-    window.addEventListener('scroll', this.handleScroll)
-    if (this.backdrop) {
-      this.backdrop.addEventListener('click', this.toggleSignInModal)
-    }
-    this.handleScroll()
-  },
-  mounted() {
-    window.addEventListener('scroll', this.handleScroll)
-  },
-  components: { Login, NavSuggestions },
-}
+    components: { Login, NavSuggestions },
+  }
 </script>
