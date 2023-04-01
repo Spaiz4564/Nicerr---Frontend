1ccb9f2152fb9910240caa74d7a4d3c8ff6ec324
Lidor Tal <lidortald@gmail.com>
2023-04-01 20:16:22 +0300

    fixes gig details seller

--- src/cmps/details/Overview.vue
+++ src/cmps/details/Overview.vue
@@ -3,12 +3,16 @@
     <p class="title">{{ gig.title }}</p>
     <div class="info-img flex align-center">
       <img class="logo" :src="gig.owner.imgUrl" alt="" />
-      <p class="name">{{ gig.owner.fullname }}</p>
-      <p>Level {{ gig.owner.level }} Seller</p>
-      <div class="rate flex align-center">
-        <div className="icon" v-html="getSvg('fiveStarsEmpty')"></div>
-        <p class="info-rate">5</p>
-        <p>(154)</p>
+      <div class="seller-preview flex">
+        <div class="text">
+          <p class="name">{{ gig.owner.fullname }}</p>
+          <p>Level {{ gig.owner.level }} Seller</p>
+        </div>
+        <div class="rate flex align-center">
+          <div className="icon" v-html="getSvg('fiveStarsEmpty')"></div>
+          <p class="info-rate">5</p>
+          <p>(154)</p>
+        </div>
       </div>
     </div>
     <div class="img">
