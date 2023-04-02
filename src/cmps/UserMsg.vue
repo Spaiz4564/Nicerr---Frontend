a3377a889068c115ffcbd31cbb0630f3eca950e9
Guy <guyshilon30@gmail.com>
2023-04-02 18:17:41 +0300

    s

--- src/cmps/UserMsg.vue
+++ src/cmps/UserMsg.vue
@@ -1,14 +1,32 @@
 <template>
   <div v-show="msg" class="user-msg">
-    <span>{{ msg }}</span>
+    <div class="msg-tick" v-html="getSvg('checkMark')"></div>
+    <div class="flex column">
+      <span>{{ msg }}</span>
+    <span>{{ msgPartTwo }}</span>
+    </div>
+
     <span class="close-btn" @click="msg = ''">x</span>
     <div class="line"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'user-msg',
-  props: ['msg'],
-}
+  import { svgService } from '../services/svg.service'
+  export default {
+    name: 'user-msg',
+    props: ['msg'],
+    methods: {
+      getSvg(iconName) {
+        return svgService.getSvg(iconName)
+      },
+    },
+    computed: {
+      msgPartTwo() {
+        if (this.msg) {
+          return this.msg.includes('Hey') ? "Your order's status has been changed." : ''
+        }
+      },
+    },
+  }
 </script>
